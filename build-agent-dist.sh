c1bb67f4a83052be3cadb196a5a34e4c52813187
Matthew R. Wilson <mwilson@mattwilson.org>
2024-07-03 21:08:01 -0700

    Add additional Linux ARM build variants

--- build-agent-dist.sh
+++ build-agent-dist.sh
@@ -14,8 +14,17 @@ mkdir -p ${BASEPATH}{linux-amd64,linux-armv7,windows-amd64,macos-amd64,macos-aar
 echo "Building for linux/amd64..."
 GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -trimpath -o ${BASEPATH}linux-amd64/virtual1403 github.com/racingmars/virtual1403/agent
 
+echo "Building for linux/armv6..."
+GOOS=linux GOARCH=arm GOARM=6 go build -ldflags "-X main.version=$VERSION" -trimpath -o ${BASEPATH}linux-armv6/virtual1403 github.com/racingmars/virtual1403/agent
+
+echo "Building for linux/arm64v6..."
+GOOS=linux GOARCH=arm64 GOARM=6 go build -ldflags "-X main.version=$VERSION" -trimpath -o ${BASEPATH}linux-arm64v6/virtual1403 github.com/racingmars/virtual1403/agent
+
 echo "Building for linux/armv7 (e.g. Raspberry Pi)..."
 GOOS=linux GOARCH=arm GOARM=7 go build -ldflags "-X main.version=$VERSION" -trimpath -o ${BASEPATH}linux-armv7/virtual1403 github.com/racingmars/virtual1403/agent
+
+echo "Building for linux/arm64v7 (e.g. Raspberry Pi 64-bit)..."
+GOOS=linux GOARCH=arm64 GOARM=7 go build -ldflags "-X main.version=$VERSION" -trimpath -o ${BASEPATH}linux-arm64v7/virtual1403 github.com/racingmars/virtual1403/agent
 
 echo "Building for windows/amd64..."
 GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -trimpath -o ${BASEPATH}windows-amd64/virtual1403.exe github.com/racingmars/virtual1403/agent
@@ -26,7 +35,7 @@ GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -trimpath 
 echo "Building for ARM Mac..."
 GOOS=darwin GOARCH=arm64 go build -ldflags "-X main.version=$VERSION" -trimpath -o ${BASEPATH}macos-aarch64/virtual1403 github.com/racingmars/virtual1403/agent
 
-for x in linux-amd64 linux-armv7 windows-amd64 macos-amd64 macos-aarch64; do
+for x in linux-amd64 linux-armv7 linux-arm64v7 linux-armv6 linux-arm64v6 windows-amd64 macos-amd64 macos-aarch64; do
     cp agent/config.sample.yaml ${BASEPATH}${x}/config.yaml
     cp agent/README ${BASEPATH}${x}
     cp COPYING ${BASEPATH}${x}
@@ -45,7 +54,7 @@ for x in windows-amd64 macos-amd64 macos-aarch64; do
 done
 
 # tarballs for Linux
-for x in linux-amd64 linux-armv7; do
+for x in linux-amd64 linux-armv7 linux-arm64v7 linux-armv6 linux-arm64v6; do
     cd dist/v$VERSION
     tar czf virtual1403-agent-v${VERSION}_${x}.tgz virtual1403-agent-v${VERSION}_${x}
     cd ../..
